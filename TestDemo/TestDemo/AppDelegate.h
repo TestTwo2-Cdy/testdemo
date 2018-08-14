19fbed81165ddbb90238ac35d0fa02935ad79ff1
Cdy9581 <1016205585@qq.com>
2018-08-14 19:23:37 +0800

    textdemo

---
+++ TestDemo/TestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDemo
+//
+//  Created by 郭镇华 on 2018/8/14.
+//  Copyright © 2018年 郭镇华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestDemo/TestDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestDemo
+//
+//  Created by 郭镇华 on 2018/8/14.
+//  Copyright © 2018年 郭镇华. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
